7537eb0ffe179f9e9886a5c80e57a271e7d70aa6
Jeff Albrecht <geospatialjeff@gmail.com>
2020-08-19 10:54:05 -0500

    add create_app method

--- stac_api/create_app.py
+++ stac_api/create_app.py
@@ -1,13 +1,20 @@
 """fastapi app creation"""
 from typing import Callable, Type
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, FastAPI
 from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from starlette.requests import Request
 
 from stac_api.clients.base import BaseTransactionsClient
+from stac_api.clients.postgres.transactions import TransactionsClient
+from stac_api.config import ApiSettings, inject_settings
+from stac_api.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 from stac_api.models import schemas
 from stac_api.models.api import APIRequest, APIResponse, DeleteCollection, DeleteItem
-from stac_api.utils.dependencies import discover_base_url
+from stac_api.resources import collection, conformance, item, mgmt
+from stac_api.utils.dependencies import READER, WRITER, discover_base_url
 
 
 # TODO: Only use one endpoint factory
@@ -118,3 +125,52 @@ def create_transactions_router(client: BaseTransactionsClient) -> APIRouter:
         ),
     )
     return router
+
+
+def create_app(settings: ApiSettings, transactions=False) -> FastAPI:
+    """Create a FastAPI app"""
+    app = FastAPI()
+    inject_settings(settings)
+
+    app.debug = settings.debug
+    app.include_router(mgmt.router)
+    app.include_router(conformance.router)
+    app.include_router(collection.router)
+    app.include_router(item.router)
+    add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
+    if transactions:
+        transaction_client = TransactionsClient()
+        app.include_router(create_transactions_router(transaction_client))
+
+    @app.on_event("startup")
+    async def on_startup():
+        """Create database engines and sessions on startup"""
+        app.state.ENGINE_READER = create_engine(settings.reader_connection_string)
+        app.state.ENGINE_WRITER = create_engine(settings.writer_connection_string)
+        app.state.DB_READER = sessionmaker(
+            autocommit=False, autoflush=False, bind=app.state.ENGINE_READER
+        )
+        app.state.DB_WRITER = sessionmaker(
+            autocommit=False, autoflush=False, bind=app.state.ENGINE_WRITER
+        )
+
+    @app.on_event("shutdown")
+    async def on_shutdown():
+        """Dispose of database engines and sessions on app shutdown"""
+        app.state.ENGINE_READER.dispose()
+        app.state.ENGINE_WRITER.dispose()
+
+    @app.middleware("http")
+    async def create_db_connection(request: Request, call_next):
+        """Create a new database connection for each request"""
+        reader = request.app.state.DB_READER()
+        writer = request.app.state.DB_WRITER()
+        READER.set(reader)
+        WRITER.set(writer)
+        resp = await call_next(request)
+        reader.close()
+        writer.close()
+        return resp
+
+    return app
