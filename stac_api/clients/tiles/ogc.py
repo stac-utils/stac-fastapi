aa5f1cc1d744ffe3f1ec37ce86dbb4ac83f7c56a
Jeff Albrecht <geospatialjeff@gmail.com>
2020-08-24 10:51:34 -0500

    finish tiles client

--- stac_api/clients/tiles/ogc.py
+++ stac_api/clients/tiles/ogc.py
@@ -1,6 +1,10 @@
 """ogc tiles client"""
 
 from dataclasses import dataclass
+from typing import Union
+from urllib.parse import urljoin
+
+from starlette.responses import RedirectResponse
 
 from stac_api.clients.postgres.item import ItemCrudClient
 from stac_api.models.links import TileLinks
@@ -13,13 +17,28 @@ from stac_pydantic.collection import SpatialExtent
 class TilesClient(ItemCrudClient):
     """OGC Tiles specific operations"""
 
-    def get_item_tiles(self, id: str, **kwargs) -> TileSetResource:
+    def get_item_tiles(
+        self, id: str, **kwargs
+    ) -> Union[RedirectResponse, TileSetResource]:
         """Get OGC TileSet resource for a stac item"""
         item = self.get_item(id, **kwargs)
-        return TileSetResource(
+        resource = TileSetResource(
             extent=SpatialExtent(bbox=[list(item.bbox)]),
             title=f"Tiled layer of {item.collection}/{item.id}",
             links=TileLinks(
-                item_id=item.id, collection_id=item.collection
+                item_id=item.id,
+                collection_id=item.collection,
+                base_url=kwargs["base_url"],
             ).create_links(),
         )
+
+        if "text/html" in kwargs["request"].headers["accept"]:
+            item_uri = urljoin(
+                kwargs["base_url"], f"collections/{item.collection}/items/{item.id}"
+            )
+            redirect_url = (
+                f"{urljoin(kwargs['base_url'], '/stac/viewer')}?url={item_uri}"
+            )
+            return RedirectResponse(redirect_url)
+
+        return resource
