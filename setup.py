6cbcb25243cc39fe47acb818b3bab152a5fb641b
Jeff Albrecht <geospatialjeff@gmail.com>
2020-09-25 17:35:51 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,56 @@
+"""arturo-stac-api"""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    desc = f.read()
+
+
+install_requires = [
+    "uvicorn>=0.11.7",
+    "gunicorn",
+    "fastapi",
+    "alembic",
+    "psycopg2-binary",
+    "shapely",
+    "sqlalchemy",
+    "geoalchemy2<0.8.0",
+    "sqlakeyset",
+    "stac-pydantic>=1.3.5",
+    "pydantic[dotenv]",
+    "cogeo-mosaic==3.0a10",
+    "titiler==0.1a2",
+]
+
+extra_reqs = {
+    "dev": ["pytest", "pytest-cov", "pytest-asyncio", "pre-commit", "requests"],
+}
+
+
+setup(
+    name="arturo-stac-api",
+    description="Arturo's STAC compliant API implementation",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    version="0.2.0",
+    python_requires=">=3.8",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="stac fastapi cog",
+    author=u"Arturo Engineering",
+    author_email="engineering@arturo.ai",
+    url="https://github.com/arturo-ai/arturo-stac-api",
+    license="MIT",
+    packages=find_packages(exclude=["alembic", "tests", "scripts"]),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=extra_reqs["dev"],
+    extras_require=extra_reqs,
+)
