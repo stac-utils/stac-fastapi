ae2ce5cc60f606f418c3fa4010a4386bff54a80e
Jeff Albrecht <geospatialjeff@gmail.com>
2020-10-27 18:23:03 -0500

    add query extension

---
+++ stac_api/api/extensions/query.py
@@ -0,0 +1,18 @@
+"""query extension"""
+from dataclasses import dataclass
+
+from fastapi import FastAPI
+
+from stac_api.api.extensions import ApiExtension
+
+
+@dataclass
+class QueryExtension(ApiExtension):
+    """
+    stac-api query extension
+    (https://github.com/radiantearth/stac-api-spec/tree/master/extensions/query)
+    """
+
+    def register(self, app: FastAPI) -> None:
+        """register extension with the application"""
+        pass
