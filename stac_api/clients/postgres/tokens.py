6300108e3ed827cc936d2f908d27613ed02c41cd
Jeff Albrecht <geospatialjeff@gmail.com>
2021-01-23 21:36:28 -0600

    update pagination client

--- stac_api/clients/postgres/tokens.py
+++ stac_api/clients/postgres/tokens.py
@@ -1,5 +1,6 @@
 """Pagination token client."""
 
+import logging
 import os
 from base64 import urlsafe_b64encode
 from typing import Type
@@ -7,8 +8,10 @@ from typing import Type
 import attr
 
 from stac_api.clients.postgres.base import PostgresClient
-from stac_api.errors import DatabaseError
+from stac_api.errors import DatabaseError, NotFoundError
 from stac_api.models import database
+
+logger = logging.getLogger(__name__)
 
 
 @attr.s
@@ -21,22 +24,33 @@ class PaginationTokenClient(PostgresClient):
         """Insert a keyset into the database"""
         # uid has collision chance of 1e-7 percent
         uid = urlsafe_b64encode(os.urandom(6)).decode()
-        try:
-            token = database.PaginationToken(id=uid, keyset=keyset)
-            self.writer_session.add(token)
-            self.commit()
-            return uid
-        except DatabaseError:
-            # Try again if uid already exists in the database
-            # TODO: Explicitely check for ConflictError (if insert fails for other reasons it should be raised)
-            self.insert(keyset, tries=tries + 1)
-            if tries > 5:
-                raise
+        with self.session.writer.context_session() as session:
+            try:
+                token = database.PaginationToken(id=uid, keyset=keyset)
+                session.add(token)
+                return uid
+            except DatabaseError:
+                # Try again if uid already exists in the database
+                # TODO: Explicitely check for ConflictError (if insert fails for other reasons it should be raised)
+                if tries > 5:
+                    raise
+                self.insert(keyset, tries=tries + 1)
 
     def get(self, token_id: str) -> str:
         """Retrieve a keyset from the database"""
-        row = self.lookup_id(token_id).first()
-        return row.keyset
+        with self.session.reader.context_session() as session:
+            try:
+                token = (
+                    session.query(self.table).filter(self.table.id == token_id).first()
+                )
+            except Exception as e:
+                logger.error(e, exc_info=True)
+                raise DatabaseError("Error fetching token from database")
+
+            if not token:
+                raise NotFoundError(f"Could not find token {token_id}")
+
+            return token.keyset
 
 
 def pagination_token_client_factory() -> PaginationTokenClient:
