e1d8ab7b7b1e15210341e08c518e4ccc17f267a4
Jeff Albrecht <geospatialjeff@gmail.com>
2021-01-23 22:46:45 -0600

    refactor transactions

--- stac_api/clients/postgres/transactions.py
+++ stac_api/clients/postgres/transactions.py
@@ -6,109 +6,74 @@ from typing import Dict, List, Optional, Type, Union
 
 import attr
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session as SqlSession
 
 from stac_api import errors
 from stac_api.clients.base import BaseTransactionsClient, BulkTransactionsClient
-from stac_api.clients.postgres.base import PostgresClient
+from stac_api.clients.postgres.session import Session
 from stac_api.models import database, schemas
+from stac_api.errors import DatabaseError, NotFoundError
 
 logger = logging.getLogger(__name__)
 
 
 @attr.s
-class TransactionsClient(PostgresClient, BaseTransactionsClient):
+class TransactionsClient(BaseTransactionsClient):
     """Transactions extension specific CRUD operations"""
 
+    session: Session = attr.ib(default=attr.Factory(Session.create_from_env))
     collection_table: Type[database.Collection] = attr.ib(default=database.Collection)
     item_table: Type[database.Item] = attr.ib(default=database.Item)
 
-    def _create(
-        self,
-        model: Union[schemas.Collection, schemas.Item],
-        table: Union[Type[database.Collection], Type[database.Item]],
-    ) -> Union[database.Collection, database.Item]:
-        """Create a single record"""
-        try:
-            self.lookup_id(model.id, table=table)
-            error_message = f"Row {model.id} already exists"
-            logger.error(error_message, exc_info=True)
-            raise errors.ConflictError(error_message)
-        except errors.NotFoundError:
-            row_data = table.from_schema(model)
-            self.writer_session.add(row_data)
-            self.commit()
-            return row_data
-
-    def _update(
-        self,
-        model: Union[schemas.Collection, schemas.Item],
-        table: Union[Type[database.Collection], Type[database.Item]],
-    ) -> Union[database.Collection, database.Item]:
-        """Create a single record if it does not exist or update an existing record"""
-        try:
-            query = self.lookup_id(model.id, table=table)
-            update_data = table.get_database_model(model)
-            # SQLAlchemy orm updates don't seem to like geoalchemy types
-            update_data.pop("geometry", None)
-            query.update(update_data)
-            self.commit()
-            return table.from_schema(model)
-        except errors.NotFoundError:
-            row_data = table.from_schema(model)
-            self.writer_session.add(row_data)
-            self.commit()
-            return row_data
-
-    def _delete(
-        self, item_id: str, table: Union[Type[database.Collection], Type[database.Item]]
-    ) -> Union[database.Collection, database.Item]:
-        """Delete a single record"""
-        query = self.lookup_id(item_id, table=table)
-        row_data = query.first()
-        query.delete()
-        self.commit()
-        return row_data
-
     def create_item(self, model: schemas.Item, **kwargs) -> schemas.Item:
-        """Create an item"""
-        obj = self._create(model, table=self.item_table)
-        obj.base_url = str(kwargs["request"].base_url)
-        return schemas.Item.from_orm(obj)
-
-    def update_item(self, model: schemas.Item, **kwargs) -> schemas.Item:
-        """Update an item"""
-        obj = self._update(model, table=self.item_table)
-        obj.base_url = str(kwargs["request"].base_url)
-        return schemas.Item.from_orm(obj)
-
-    def delete_item(self, id: str, **kwargs) -> schemas.Item:
-        """Delete an item"""
-        obj = self._delete(id, table=self.item_table)
-        obj.base_url = str(kwargs["request"].base_url)
-        return schemas.Item.from_orm(obj)
+        data = self.item_table.from_schema(model)
+        with self.session.writer.context_session() as session:
+            session.add(data)
+        return model
 
     def create_collection(
         self, model: schemas.Collection, **kwargs
     ) -> schemas.Collection:
-        """Create a collection"""
-        obj = self._create(model, table=self.collection_table)
-        obj.base_url = str(kwargs["request"].base_url)
-        return schemas.Collection.from_orm(obj)
+        data = self.collection_table.from_schema(model)
+        with self.session.writer.context_session() as session:
+            session.add(data)
+        return model
+
+    def update_item(self, model: schemas.Item, **kwargs) -> schemas.Item:
+        with self.session.reader.context_session() as session:
+            query = session.query(self.item_table).filter(self.item_table.id == model.id)
+            if not query.scalar():
+                raise NotFoundError(f"Item {model.id} not found")
+            # SQLAlchemy orm updates don't seem to like geoalchemy types
+            data = self.item_table.get_database_model(model)
+            data.pop("geometry", None)
+        return model
 
     def update_collection(
         self, model: schemas.Collection, **kwargs
     ) -> schemas.Collection:
-        """Update a collection"""
-        obj = self._update(model, table=self.collection_table)
-        obj.base_url = str(kwargs["request"].base_url)
-        return schemas.Collection.from_orm(obj)
+        with self.session.reader.context_session() as session:
+            query = session.query(self.item_table).filter(self.item_table.id == model.id)
+            if not query.scalar():
+                raise NotFoundError(f"Item {model.id} not found")
+            # SQLAlchemy orm updates don't seem to like geoalchemy types
+            data = self.collection_table.get_database_model(model)
+            data.pop("geometry", None)
+        return model
 
-    def delete_collection(self, id: str, **kwargs) -> schemas.Collection:
-        """Delete a collection"""
-        obj = self._delete(id, table=self.collection_table)
-        obj.base_url = str(kwargs["request"].base_url)
-        return schemas.Collection.from_orm(obj)
+    def delete_item(self, id: str, **kwargs) -> schemas.Item:
+        with self.session.writer.context_session() as session:
+            query = session.query(self.item_table).filter(self.item_table.id == id)
+            data = query.first()
+            query.delete()
+        return data
 
+    def delete_collection(self, id: str, **kwargs) -> schemas.Item:
+        with self.session.writer.context_session() as session:
+            query = session.query(self.collection_table).filter(self.item_table.id == id)
+            data = query.first()
+            query.delete()
+        return data
 
 @attr.s
 class PostgresBulkTransactions(BulkTransactionsClient):
