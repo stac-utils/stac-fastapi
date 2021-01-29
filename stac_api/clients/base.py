15ebdf92afca15843d362943c4bf04893a45e884
Kyle Barron <kylebarron2@gmail.com>
2021-01-28 21:30:38 -0700

    Fix types

--- stac_api/clients/base.py
+++ stac_api/clients/base.py
@@ -63,8 +63,10 @@ class BaseBulkTransactionsClient(abc.ABC):
             yield lst[i : i + n]
 
     @abc.abstractmethod
-    def bulk_item_insert(self, items: List[Dict], chunks: Optional[int] = None) -> None:
-        """bulk item insertion, not implemented by default, and not exposed through the api"""
+    def bulk_item_insert(
+        self, items: schemas.Items, chunks: Optional[int] = None
+    ) -> str:
+        """Bulk item insertion, not implemented by default"""
         raise NotImplementedError
 
 
