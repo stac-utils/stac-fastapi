4a999b799c447d653a03516c475cd2b8c25987d2
Rob Emanuele <rdemanuele@gmail.com>
2021-03-21 16:21:12 -0400

    Add stac_fastapi_server package.
    This brings in the logic of stac_api.app into it's own
    subpackage. This is used by the development environment to spin up
    stac-fastapi via docker-compose

---
+++ stac_fastapi_server/stac_fastapi/server/app.py
@@ -0,0 +1,34 @@
+"""FastAPI application."""
+from stac_fastapi.types.config import ApiSettings
+from stac_fastapi.api.app import StacApi
+from stac_fastapi.extensions.core import (
+    FieldsExtension,
+    QueryExtension,
+    SortExtension,
+    TransactionExtension,
+)
+from stac_fastapi.extensions.third_party import (
+    BulkTransactionExtension
+)
+from stac_fastapi.postgres.core import CoreCrudClient
+from stac_fastapi.postgres.config import PostgresSettings
+from stac_fastapi.postgres.session import Session
+from stac_fastapi.postgres.transactions import (
+    BulkTransactionsClient,
+    TransactionsClient,
+)
+
+settings = ApiSettings()
+session = Session.create_from_settings(PostgresSettings())
+api = StacApi(
+    settings=settings,
+    extensions=[
+        TransactionExtension(client=TransactionsClient(session=session)),
+        BulkTransactionExtension(client=BulkTransactionsClient(session=session)),
+        FieldsExtension(),
+        QueryExtension(),
+        SortExtension()
+    ],
+    client=CoreCrudClient(session=session),
+)
+app = api.app
