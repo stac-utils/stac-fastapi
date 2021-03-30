473c50f38a23aeed89f9ef52aa9485e0da29d62f
Jeff Albrecht <geospatialjeff@gmail.com>
2021-03-29 21:51:39 -0500

    add an async endpoint factory, same as sync but its async

--- stac_fastapi_api/stac_fastapi/api/routes.py
+++ stac_fastapi_api/stac_fastapi/api/routes.py
@@ -13,6 +13,9 @@ def create_endpoint(
 ) -> Callable:
     """Create a FastAPI endpoint.
 
+    This endpoint executes code in an external threadpool which is then awaited by the event loop.
+    https://fastapi.tiangolo.com/async/#path-operation-functions
+
     Accepts either an `stac_fastapi.api.models.APIRequest` or `pydantic.BaseModel`.
 
     Pydantic models are good for validating request bodies (ex. POST requests), and expect the signature of the
@@ -43,6 +46,53 @@ def create_endpoint(
     else:
 
         def _endpoint(
+            request: Request,
+            request_data: request_model,  # type:ignore
+        ):
+            """Endpoint."""
+            resp = func(request_data, request=request)
+            return resp
+
+    return _endpoint
+
+
+def create_async_endpoint(
+    func: Callable, request_model: Type[Union[APIRequest, BaseModel]]
+) -> Callable:
+    """Create a FastAPI endpoint.
+
+    This endpoint executes code using the event loop.
+
+    Accepts either an `stac_fastapi.api.models.APIRequest` or `pydantic.BaseModel`.
+
+    Pydantic models are good for validating request bodies (ex. POST requests), and expect the signature of the
+    callable matches that of the request model.
+
+    APIRequest is good for validating query/path parameters (ex. GET requests) and allows for FastAPI dependency
+    injection.  It is expected the that the return of `APIRequest.kwargs` matches that of the callable.
+
+    Args:
+        func: the wrapped function.
+        request_model: either `stac_fastapi.api.models.APIRequest` or `pydantic.BaseModel`.
+
+    Returns:
+        callable: fastapi route which may be added to a router/application
+    """
+    if isinstance(request_model, APIRequest):
+
+        async def _endpoint(
+            request: Request,
+            request_data: request_model = Depends(),  # type:ignore
+        ):
+            """Endpoint."""
+            resp = func(
+                request=request, **request_data.kwargs()  # type:ignore
+            )
+            return resp
+
+    else:
+
+        async def _endpoint(
             request: Request,
             request_data: request_model,  # type:ignore
         ):
