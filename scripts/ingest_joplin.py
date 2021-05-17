178a625996bd08a00e4c5ec6af0ef5731d077e9e
Alex G Rice <alex@radiant.earth>
2021-05-17 15:25:03 -0600

    Fix docker-compose usage of ingest_joplin.py. (#133)

--- scripts/ingest_joplin.py
+++ scripts/ingest_joplin.py
@@ -1,6 +1,6 @@
 """Ingest sample data during docker-compose"""
 import json
-import os
+from pathlib import Path
 from urllib.parse import urljoin
 
 import requests
@@ -9,17 +9,17 @@ bucket = "arturo-stac-api-test-data"
 app_host = "http://app:8081"
 
 
-def ingest_joplin_data(data_dir="/app/tests/data/joplin"):
+def ingest_joplin_data(data_dir=Path.cwd() / "tests" / "data" / "joplin"):
     """ingest data."""
 
-    with open(os.path.join(data_dir, "collection.json")) as f:
+    with open(data_dir / "collection.json") as f:
         collection = json.load(f)
 
     r = requests.post(urljoin(app_host, "/collections"), json=collection)
     if r.status_code not in (200, 409):
         r.raise_for_status()
 
-    with open(os.path.join(data_dir, "index.geojson")) as f:
+    with open(data_dir / "index.geojson") as f:
         index = json.load(f)
 
     for feat in index["features"]:
