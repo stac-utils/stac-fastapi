4b001dc4266c624445f40eab65a6d119af7d0233
David Bitner <bitner@dbspatial.com>
2021-06-15 08:55:55 -0500

    Apply suggestions from code review
    remove unused load_test_data fixtures
    
    Co-authored-by: Jeff Albrecht <jeff@arturo.ai>

--- stac_fastapi/pgstac/tests/clients/test_postgres.py
+++ stac_fastapi/pgstac/tests/clients/test_postgres.py
@@ -42,7 +42,7 @@ async def test_update_collection(app_client, load_test_collection):
 
 @pytest.mark.asyncio
 async def test_delete_collection(
-    app_client, load_test_data: Callable, load_test_collection
+    app_client, load_test_collection
 ):
     in_coll = load_test_collection
 
@@ -77,7 +77,7 @@ async def test_create_item(app_client, load_test_data: Callable, load_test_colle
 
 @pytest.mark.asyncio
 async def test_update_item(
-    app_client, load_test_data: Callable, load_test_collection, load_test_item
+    app_client, load_test_collection, load_test_item
 ):
     coll = load_test_collection
     item = load_test_item
