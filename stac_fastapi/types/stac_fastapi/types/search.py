d13ed995acef47d6765f0b88934907eb8ec46cdd
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-06-29 15:39:36 +0100

    adding changes from isort

--- stac_fastapi/types/stac_fastapi/types/search.py
+++ stac_fastapi/types/stac_fastapi/types/search.py
@@ -8,9 +8,7 @@ from enum import auto
 from types import DynamicClassAttribute
 from typing import Any, Callable, Dict, List, Optional, Set, Union
 
-
 from pydantic import Field, root_validator
-
 from stac_pydantic.api import Search
 from stac_pydantic.api.extensions.fields import FieldsExtension as FieldsBase
 from stac_pydantic.utils import AutoValueEnum
