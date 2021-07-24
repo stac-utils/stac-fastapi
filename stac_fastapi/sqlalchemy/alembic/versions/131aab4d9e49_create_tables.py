b850bd53aa4ffce434cc220a67db7563a5a2db62
Rob Emanuele <rdemanuele@gmail.com>
2021-07-23 22:44:35 -0400

    isort

--- stac_fastapi/sqlalchemy/alembic/versions/131aab4d9e49_create_tables.py
+++ stac_fastapi/sqlalchemy/alembic/versions/131aab4d9e49_create_tables.py
@@ -1,15 +1,14 @@
 """create initial schema
 
 Revision ID: 131aab4d9e49
-Revises: 
+Revises:
 Create Date: 2020-02-09 13:03:09.336631
 
 """  # noqa
 import sqlalchemy as sa
+from alembic import op
 from geoalchemy2.types import Geometry
 from sqlalchemy.dialects.postgresql import JSONB
-
-from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "131aab4d9e49"
