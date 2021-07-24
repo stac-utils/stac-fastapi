96332c7d13e8ee0fac062de24b2b2591089b27d3
Rob Emanuele <rdemanuele@gmail.com>
2021-07-23 23:35:10 -0400

    Update to stac-pydantic 2.0.1

--- stac_fastapi/pgstac/setup.py
+++ stac_fastapi/pgstac/setup.py
@@ -10,7 +10,7 @@ install_requires = [
     "attrs",
     "orjson",
     "pydantic[dotenv]",
-    "stac_pydantic==2.0.0",
+    "stac_pydantic==2.0.*",
     "stac-fastapi.types",
     "stac-fastapi.api",
     "stac-fastapi.extensions",
--- stac_fastapi/types/setup.py
+++ stac_fastapi/types/setup.py
@@ -9,7 +9,7 @@ install_requires = [
     "fastapi",
     "attrs",
     "pydantic[dotenv]",
-    "stac_pydantic==2.0.0",
+    "stac_pydantic==2.0.*",
 ]
 
 extra_reqs = {
