5324187c890866381b6f469b4e7d62043587c797
Jon Duckworth <duckontheweb@gmail.com>
2021-08-17 15:40:51 -0400

    Failing test for search link, use fixture for landing page fetch

--- stac_fastapi/pgstac/tests/resources/test_conformance.py
+++ stac_fastapi/pgstac/tests/resources/test_conformance.py
@@ -1,21 +1,54 @@
+import urllib.parse
+
 import pytest
 
 
+@pytest.fixture(scope="module")
+async def response(app_client):
+    return await app_client.get("/")
+
+
+@pytest.fixture(scope="module")
+async def response_json(response):
+    return response.json()
+
+
+def get_link(landing_page, rel_type):
+    return next(filter(lambda link: link["rel"] == rel_type, landing_page["links"]))
+
+
 @pytest.mark.asyncio
-async def test_landing_page(app_client):
+async def test_landing_page_health(response):
     """Test landing page"""
-    resp = await app_client.get("/")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
+
+
+@pytest.mark.asyncio
+async def test_search_link(response_json):
+    search_link = get_link(response_json, "search")
+    assert search_link.get("type") == "application/geo+json"
+
+    # This endpoint currently returns a 404 for empty result sets, but testing for this response
+    # code here seems meaningless since it would be the same as if the endpoint did not exist. Until
+    # https://github.com/stac-utils/stac-fastapi/pull/227 has been merged, we simply test that the
+    # path is correct.
+    search_path = urllib.parse.urlsplit(search_link.get("href")).path
+    assert search_path == "/search"
+
+
+@pytest.mark.asyncio
+async def test_conformance_link(response_json, app_client):
+    # Make sure conformance classes are linked
+    conf = get_link(response_json, "conformance")["href"]
+    resp = await app_client.get(conf)
     assert resp.status_code == 200
-    resp_json = resp.json()
+
+
+@pytest.mark.asyncio
+async def test_docs_link(response_json, app_client):
 
     # Make sure OpenAPI docs are linked
-    docs = next(filter(lambda link: link["rel"] == "docs", resp_json["links"]))["href"]
+    docs = get_link(response_json, "docs")["href"]
     resp = await app_client.get(docs)
-    assert resp.status_code == 200
-
-    # Make sure conformance classes are linked
-    conf = next(filter(lambda link: link["rel"] == "conformance", resp_json["links"]))[
-        "href"
-    ]
-    resp = await app_client.get(conf)
     assert resp.status_code == 200
