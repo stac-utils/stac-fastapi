60c6ee72c4eeb9c2a877bfbd05c82bc9d39d5d20
Jon Duckworth <duckontheweb@gmail.com>
2021-08-17 15:40:51 -0400

    Remove unnecessary asyncs

--- stac_fastapi/pgstac/tests/resources/test_conformance.py
+++ stac_fastapi/pgstac/tests/resources/test_conformance.py
@@ -17,24 +17,26 @@ def get_link(landing_page, rel_type):
     return next(filter(lambda link: link["rel"] == rel_type, landing_page["links"]))
 
 
-@pytest.mark.asyncio
-async def test_landing_page_health(response):
+def test_landing_page_health(response):
     """Test landing page"""
     assert response.status_code == 200
     assert response.headers["content-type"] == "application/json"
 
 
-@pytest.mark.asyncio
-async def test_search_link(response_json):
+def test_search_link(response_json):
     search_link = get_link(response_json, "search")
     assert search_link.get("type") == "application/geo+json"
+
+    search_path = urllib.parse.urlsplit(search_link.get("href")).path
+    assert search_path == "/search"
 
     # This endpoint currently returns a 404 for empty result sets, but testing for this response
     # code here seems meaningless since it would be the same as if the endpoint did not exist. Until
     # https://github.com/stac-utils/stac-fastapi/pull/227 has been merged, we simply test that the
     # path is correct.
-    search_path = urllib.parse.urlsplit(search_link.get("href")).path
-    assert search_path == "/search"
+    #
+    # resp = await app_client.get(search_path)
+    # assert resp.status_code == 200
 
 
 @pytest.mark.asyncio
