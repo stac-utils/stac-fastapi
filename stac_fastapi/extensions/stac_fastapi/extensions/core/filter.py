560f3e411a2e80bd7ed4a48c0bd059a1eea672ed
Rob Emanuele <rdemanuele@gmail.com>
2021-08-18 21:07:07 -0400

    Update filter extension conformance classes

--- stac_fastapi/extensions/stac_fastapi/extensions/core/filter.py
+++ stac_fastapi/extensions/stac_fastapi/extensions/core/filter.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 """Filter Extension."""
-
+from enum import Enum
 from typing import Callable, List, Type, Union
 
 import attr
@@ -11,6 +11,30 @@ from stac_fastapi.api.models import APIRequest, CollectionUri, EmptyRequest
 from stac_fastapi.api.routes import create_async_endpoint, create_sync_endpoint
 from stac_fastapi.types.core import AsyncBaseFiltersClient, BaseFiltersClient
 from stac_fastapi.types.extension import ApiExtension
+
+
+class FilterConformanceClasses(str, Enum):
+    """Conformance classes for the Filter extension.
+
+    See https://github.com/radiantearth/stac-api-spec/tree/v1.0.0-beta.3/fragments/filter
+    """
+
+    FILTER = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:filter"
+    ITEM_SEARCH_FILTER = (
+        "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:item-search-filter"
+    )
+    CQL_TEXT = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:cql-text"
+    CQL_JSON = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:cql-json"
+    BASIC_CQL = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:basic-cql"
+    BASIC_SPATIAL_OPERATORS = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:basic-spatial-operators"
+    BASIC_TEMPORAL_OPERATORS = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:basic-temporal-operators"
+    ENHANCED_COMPARISON_OPERATORS = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:enhanced-comparison-operators"
+    ENHANCED_SPATIAL_OPERATORS = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:enhanced-spatial-operators"
+    ENHANCED_TEMPORAL_OPERATORS = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:enhanced-temporal-operators"
+    FUNCTIONS = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:functions"
+    ARITHMETIC = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:arithmetic"
+    ARRAYS = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:arrays"
+    QUERYABLE_SECOND_OPERAND = "https://api.stacspec.org/v1.0.0-beta.3/item-search#filter:queryable-second-operand"
 
 
 @attr.s
@@ -35,9 +59,10 @@ class FilterExtension(ApiExtension):
     )
     conformance_classes: List[str] = attr.ib(
         default=[
-            "https://api.stacspec.org/v1.0.0-beta.2/item-search#filter",
-            "https://api.stacspec.org/v1.0.0-beta.2/item-search#filter:simple-cql",
-            "https://api.stacspec.org/v1.0.0-beta.2/item-search#filter:item-search-filter",
+            FilterConformanceClasses.FILTER,
+            FilterConformanceClasses.ITEM_SEARCH_FILTER,
+            FilterConformanceClasses.BASIC_CQL,
+            FilterConformanceClasses.CQL_TEXT,
         ]
     )
     router: APIRouter = attr.ib(factory=APIRouter)
