27e134589107654920c2f1dba54773c8c85d4e1a
Rob Emanuele <rdemanuele@gmail.com>
2021-09-23 11:35:09 -0400

    Set collection_id on search for pgstac get_item; update to pgstac 0.3.4 (#270)
    * Update to pgstac 0.3.4
    
    * Include collection_id in get_item search.
    
    I've seen strange behavior described in
    https://github.com/stac-utils/pgstac/issues/58, where searching only
    by an Item ID was returning the incorrect Item. Specifyng the
    Collection ID, which this method should be doing, solved this in my case.
    
    * Fix NotFoundError message
    
    * Update CHANGELOG

--- stac_fastapi/pgstac/setup.py
+++ stac_fastapi/pgstac/setup.py
@@ -25,7 +25,7 @@ extra_reqs = {
         "pytest-asyncio",
         "pre-commit",
         "requests",
-        "pypgstac==0.3.3",
+        "pypgstac==0.3.4",
         "httpx",
         "shapely",
     ],
--- stac_fastapi/pgstac/stac_fastapi/pgstac/core.py
+++ stac_fastapi/pgstac/stac_fastapi/pgstac/core.py
@@ -207,10 +207,12 @@ class CoreCrudClient(AsyncBaseCoreClient):
         # If collection does not exist, NotFoundError wil be raised
         await self.get_collection(collection_id, **kwargs)
 
-        req = PgstacSearch(ids=[item_id], limit=1)
+        req = PgstacSearch(ids=[item_id], collections=[collection_id], limit=1)
         item_collection = await self._search_base(req, **kwargs)
         if not item_collection["features"]:
-            raise NotFoundError(f"Collection {collection_id} does not exist.")
+            raise NotFoundError(
+                f"Item {item_id} in Collection {collection_id} does not exist."
+            )
 
         return Item(**item_collection["features"][0])
 
