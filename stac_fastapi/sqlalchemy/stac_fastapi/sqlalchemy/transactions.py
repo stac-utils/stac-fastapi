fa8e6c32c05ea8cf8a9920cb2af71fd4f6f1da7e
jonhealy1 <jonathan.d.healy@gmail.com>
2021-11-29 07:00:07 -0800

    clean up

--- stac_fastapi/sqlalchemy/stac_fastapi/sqlalchemy/transactions.py
+++ stac_fastapi/sqlalchemy/stac_fastapi/sqlalchemy/transactions.py
@@ -62,7 +62,9 @@ class TransactionsClient(BaseTransactionsClient):
                 self.item_table.collection_id == model["collection"]
             )
             if not query.scalar():
-                raise NotFoundError(f"Item {model['id']} in collection {model['collection']}")
+                raise NotFoundError(
+                    f"Item {model['id']} in collection {model['collection']}"
+                )
             # SQLAlchemy orm updates don't seem to like geoalchemy types
             db_model = self.item_serializer.stac_to_db(model)
             query.update(self.item_serializer.row_to_dict(db_model))
@@ -94,11 +96,15 @@ class TransactionsClient(BaseTransactionsClient):
         """Delete item."""
         base_url = str(kwargs["request"].base_url)
         with self.session.writer.context_session() as session:
+            query = session.query(self.item_table).filter(
+                self.item_table.collection_id == collection_id
+            )
             query = session.query(self.item_table).filter(self.item_table.id == item_id)
-            # query = session.query(self.item_table).filter(self.item_table.collection_id == collection_id)
             data = query.first()
             if not data:
-                raise NotFoundError(f"Item {item_id} not found in collection {collection_id}")
+                raise NotFoundError(
+                    f"Item {item_id} not found in collection {collection_id}"
+                )
             query.delete()
             return self.item_serializer.db_to_stac(data, base_url=base_url)
 
