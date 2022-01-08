03b4c3223627638680e3af5f6d8e2191d472e1ef
jonhealy1 <jonathan.d.healy@gmail.com>
2022-01-08 00:16:53 -0800

    clean up transactions

--- stac_fastapi/mongo/stac_fastapi/mongo/transactions.py
+++ stac_fastapi/mongo/stac_fastapi/mongo/transactions.py
@@ -1,17 +1,12 @@
 """transactions extension client."""
 
 import logging
-from typing import Optional, Type
-
 import attr
-from pymongo import collection
 
 from stac_fastapi.extensions.third_party.bulk_transactions import (
     BaseBulkTransactionsClient,
     Items,
 )
-from stac_fastapi.sqlalchemy import serializers
-from stac_fastapi.sqlalchemy.models import database
 from stac_fastapi.sqlalchemy.session import Session
 from stac_fastapi.types import stac as stac_types
 from stac_fastapi.types.core import BaseTransactionsClient
@@ -25,19 +20,10 @@ logger = logging.getLogger(__name__)
 @attr.s
 class TransactionsClient(BaseTransactionsClient):
     """Transactions extension specific CRUD operations."""
-
     session: Session = attr.ib(default=attr.Factory(Session.create_from_env))
-    collection_table: Type[database.Collection] = attr.ib(default=database.Collection)
-    item_table: Type[database.Item] = attr.ib(default=database.Item)
-    item_serializer: Type[serializers.Serializer] = attr.ib(
-        default=serializers.ItemSerializer
-    )
-    collection_serializer: Type[serializers.Serializer] = attr.ib(
-        default=serializers.CollectionSerializer
-    )
     db = MongoSettings()
 
-    def create_item(self, model: stac_types.Item, **kwargs) -> stac_types.Item:
+    def create_item(self, model: stac_types.Item, **kwargs):
         """Create item."""
         try:
             base_url = str(kwargs["request"].base_url)
@@ -49,9 +35,7 @@ class TransactionsClient(BaseTransactionsClient):
             pass
         self.db.stac_item.insert_one(model)
 
-    def create_collection(
-        self, model: stac_types.Collection, **kwargs
-    ) -> stac_types.Collection:
+    def create_collection(self, model: stac_types.Collection, **kwargs):
         """Create collection."""
         try:
             base_url = str(kwargs["request"].base_url)
@@ -63,33 +47,27 @@ class TransactionsClient(BaseTransactionsClient):
             pass
         self.db.stac_collection.insert_one(model)
 
-    def update_item(self, model: stac_types.Item, **kwargs) -> stac_types.Item:
+    def update_item(self, model: stac_types.Item, **kwargs):
         """Update item."""
         item = self.db.stac_item.find_one({'id': model["id"], "collection": model["collection"]})
         if not item:
             raise NotFoundError(f"Item {model['id']} in collection {model['collection']} not found")
-
         self.delete_item(model)
         self.create_item(model)
       
-    def update_collection(
-        self, model: stac_types.Collection, **kwargs
-    ) -> stac_types.Collection:
+    def update_collection(self, model: stac_types.Collection, **kwargs):
         """Update collection."""
         collection = self.db.stac_collection.find_one({'id': model["id"]})
         if not collection:
             raise NotFoundError(f"Collection {model['collection']} not found")
-
         self.delete_collection(model)
         self.create_collection(model)
 
-    def delete_item(
-        self, item_id: str, collection_id: str, **kwargs
-    ) -> stac_types.Item:
+    def delete_item(self, item_id: str, collection_id: str, **kwargs):
         """Delete item."""
         self.db.stac_item.delete_one({'id': item_id, "collection": collection_id})
        
-    def delete_collection(self, id: str, **kwargs) -> stac_types.Collection:
+    def delete_collection(self, id: str, **kwargs):
         """Delete collection."""
         self.db.stac_item.delete_one({'id': id})
 
