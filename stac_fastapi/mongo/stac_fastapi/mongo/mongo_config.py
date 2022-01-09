c3754415e5554c8b0fa1349d9b958421017a676d
jonhealy1 <jonathan.d.healy@gmail.com>
2022-01-08 23:51:54 -0800

    clean up mongo_config

--- stac_fastapi/mongo/stac_fastapi/mongo/mongo_config.py
+++ stac_fastapi/mongo/stac_fastapi/mongo/mongo_config.py
@@ -1,4 +1,3 @@
-# import the MongoClient class
 from pymongo import MongoClient, errors
 import os
 
@@ -7,9 +6,7 @@ PORT = os.getenv("MONGO_PORT")
 
 def MongoSettings():
 
-    # use a try-except indentation to catch MongoClient() errors
     try:
-        # try to instantiate a client instance
         client = MongoClient(
             host = [ str(DOMAIN) + ":" + str(PORT) ],
             serverSelectionTimeoutMS = 3000, # 3 second timeout
@@ -17,18 +14,13 @@ def MongoSettings():
             password = os.getenv("MONGO_PASS"),
         )
 
-        # print the version of MongoDB server if connection successful
         print ("server version:", client.server_info()["version"])
-
-        # get the database_names from the MongoClient()
         database_names = client.list_database_names()
-
         print ("\ndatabases:", database_names)
 
         return client.stac
 
     except errors.ServerSelectionTimeoutError as err:
-        # set the client and DB name list to 'None' and `[]` if exception
         client = None
         database_names = []
 
