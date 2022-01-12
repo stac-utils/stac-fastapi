f359080e370630c7c1aee5f0b2edfcb260a9612e
jonhealy1 <jonathan.d.healy@gmail.com>
2022-01-11 22:13:01 -0800

    add query extension to mongo

--- stac_fastapi/mongo/stac_fastapi/mongo/app.py
+++ stac_fastapi/mongo/stac_fastapi/mongo/app.py
@@ -16,7 +16,7 @@ from stac_fastapi.mongo.session import Session
 from stac_fastapi.mongo.transactions import (  # BulkTransactionsClient,
     TransactionsClient,
 )
-from stac_fastapi.sqlalchemy.extensions import QueryExtension
+from stac_fastapi.mongo.extensions import QueryExtension
 
 settings = MongoSettings()
 session = Session.create_from_settings(settings)
---
+++ stac_fastapi/mongo/stac_fastapi/mongo/extensions/__init__.py
@@ -0,0 +1,5 @@
+"""sqlalchemy extensions modifications."""
+
+from .query import Operator, QueryableTypes, QueryExtension
+
+__all__ = ["Operator", "QueryableTypes", "QueryExtension"]
