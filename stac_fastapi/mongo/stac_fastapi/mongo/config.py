8c2b45cd455180ae240858273905832b2eb59425
jonhealy1 <jonathan.d.healy@gmail.com>
2022-01-17 23:41:33 -0800

    run pre commit

--- stac_fastapi/mongo/stac_fastapi/mongo/config.py
+++ stac_fastapi/mongo/stac_fastapi/mongo/config.py
@@ -1,17 +1,18 @@
 """API configuration."""
+import os
 from typing import Set
 
-from stac_fastapi.types.config import ApiSettings
-import os
-
 from pymongo import MongoClient, errors
+
+from stac_fastapi.types.config import ApiSettings
 
 DOMAIN = os.getenv("MONGO_HOST")
 PORT = os.getenv("MONGO_PORT")
 
+
 class MongoSettings(ApiSettings):
     """API settings."""
-    
+
     # Fields which are defined by STAC but not included in the database model
     forbidden_fields: Set[str] = {"type"}
 
@@ -20,6 +21,7 @@ class MongoSettings(ApiSettings):
 
     @property
     def create_client(self):
+        """Create mongo client."""
         try:
             client = MongoClient(
                 host=[str(DOMAIN) + ":" + str(PORT)],
