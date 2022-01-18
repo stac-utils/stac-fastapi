bbffd0573833adcbdec00e953ec9d3aee7040074
jonhealy1 <jonathan.d.healy@gmail.com>
2022-01-18 00:44:14 -0800

    clean up session

--- stac_fastapi/mongo/stac_fastapi/mongo/session.py
+++ stac_fastapi/mongo/stac_fastapi/mongo/session.py
@@ -14,26 +14,23 @@ class FastAPISessionMaker(_FastAPISessionMaker):
     @contextmanager
     def context_session(self):
         """Override base method to include exception handling."""
-        pass
+        ...
 
 
 @attr.s
 class Session:
     """Database session management."""
 
-    reader_conn_string: str = attr.ib()
-    writer_conn_string: str = attr.ib()
-
     @classmethod
     def create_from_env(cls):
         """Create from environment."""
-        pass
+        ...
 
     @classmethod
     def create_from_settings(cls, settings):
         """Create a Session object from settings."""
-        pass
+        ...
 
     def __attrs_post_init__(self):
         """Post init handler."""
-        pass
+        ...
