e22a47e2c7710b5d98c1d36ab1e69e2bd86681de
jonhealy1 <jonathan.d.healy@gmail.com>
2022-01-17 23:36:22 -0800

    move settings into config

--- stac_fastapi/mongo/stac_fastapi/mongo/config.py
+++ stac_fastapi/mongo/stac_fastapi/mongo/config.py
@@ -2,13 +2,34 @@
 from typing import Set
 
 from stac_fastapi.types.config import ApiSettings
+import os
 
+from pymongo import MongoClient, errors
+
+DOMAIN = os.getenv("MONGO_HOST")
+PORT = os.getenv("MONGO_PORT")
 
 class MongoSettings(ApiSettings):
     """API settings."""
-
+    
     # Fields which are defined by STAC but not included in the database model
     forbidden_fields: Set[str] = {"type"}
 
     # Fields which are item properties but indexed as distinct fields in the database model
     indexed_fields: Set[str] = {"datetime"}
+
+    @property
+    def create_client(self):
+        try:
+            client = MongoClient(
+                host=[str(DOMAIN) + ":" + str(PORT)],
+                serverSelectionTimeoutMS=3000,
+                username=os.getenv("MONGO_USER"),
+                password=os.getenv("MONGO_PASS"),
+            )
+
+        except errors.ServerSelectionTimeoutError as err:
+            client = None
+            print("pymongo ERROR:", err)
+
+        return client
