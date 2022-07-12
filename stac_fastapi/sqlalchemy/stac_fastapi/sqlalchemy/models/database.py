ba3ea09264aedfe910b635d6b3a07eba6bc8d0a0
Daniel Wiesmann <daniel@tesselo.com>
2022-07-12 09:14:56 +0100

    Make item geometry and bbox nullable for sqlalchemy backend
    Closes #350

--- stac_fastapi/sqlalchemy/stac_fastapi/sqlalchemy/models/database.py
+++ stac_fastapi/sqlalchemy/stac_fastapi/sqlalchemy/models/database.py
@@ -64,8 +64,10 @@ class Item(BaseModel):  # type:ignore
     id = sa.Column(sa.VARCHAR(1024), nullable=False, primary_key=True)
     stac_version = sa.Column(sa.VARCHAR(300))
     stac_extensions = sa.Column(sa.ARRAY(sa.VARCHAR(300)), nullable=True)
-    geometry = sa.Column(GeojsonGeometry("GEOMETRY", srid=4326, spatial_index=True))
-    bbox = sa.Column(sa.ARRAY(sa.NUMERIC), nullable=False)
+    geometry = sa.Column(
+        GeojsonGeometry("GEOMETRY", srid=4326, spatial_index=True), nullable=True
+    )
+    bbox = sa.Column(sa.ARRAY(sa.NUMERIC), nullable=True)
     properties = sa.Column(JSONB)
     assets = sa.Column(JSONB)
     collection_id = sa.Column(
