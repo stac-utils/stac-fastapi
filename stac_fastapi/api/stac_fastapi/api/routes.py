d1daa927249f52efab8ce5f64827493e8d029749
Jeff Albrecht <geospatialjeff@gmail.com>
2022-10-02 16:11:50 -0400

    remove create_sync_endpoint, run sync functions in background thread (#471)
    * remove create_sync_endpoint, run sync functions in background thread
    
    * remove _create_endpoint instance methods
    
    * pass response class to filter extension endpoints

--- stac_fastapi/api/stac_fastapi/api/routes.py
+++ stac_fastapi/api/stac_fastapi/api/routes.py
@@ -1,9 +1,12 @@
 """route factories."""
+import functools
+import inspect
 from typing import Any, Callable, Dict, List, Optional, Type, TypedDict, Union
 
 from fastapi import Depends, params
 from fastapi.dependencies.utils import get_parameterless_sub_dependant
 from pydantic import BaseModel
+from starlette.concurrency import run_in_threadpool
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 from starlette.routing import BaseRoute, Match
@@ -21,12 +24,28 @@ def _wrap_response(resp: Any, response_class: Type[Response]) -> Response:
         return Response(status_code=HTTP_204_NO_CONTENT)
 
 
+def sync_to_async(func):
+    """Run synchronous function asynchronously in a background thread."""
+
+    @functools.wraps(func)
+    async def run(*args, **kwargs):
+        return await run_in_threadpool(func, *args, **kwargs)
+
+    return run
+
+
 def create_async_endpoint(
     func: Callable,
     request_model: Union[Type[APIRequest], Type[BaseModel], Dict],
     response_class: Type[Response] = JSONResponse,
 ):
-    """Wrap a coroutine in another coroutine which may be used to create a FastAPI endpoint."""
+    """Wrap a function in a coroutine which may be used to create a FastAPI endpoint.
+
+    Synchronous functions are executed asynchronously using a background thread.
+    """
+    if not inspect.iscoroutinefunction(func):
+        func = sync_to_async(func)
+
     if issubclass(request_model, APIRequest):
 
         async def _endpoint(
@@ -59,44 +78,6 @@ def create_async_endpoint(
             return _wrap_response(
                 await func(request_data, request=request), response_class
             )
-
-    return _endpoint
-
-
-def create_sync_endpoint(
-    func: Callable,
-    request_model: Union[Type[APIRequest], Type[BaseModel], Dict],
-    response_class: Type[Response] = JSONResponse,
-):
-    """Wrap a function in another function which may be used to create a FastAPI endpoint."""
-    if issubclass(request_model, APIRequest):
-
-        def _endpoint(
-            request: Request,
-            request_data: request_model = Depends(),  # type:ignore
-        ):
-            """Endpoint."""
-            return _wrap_response(
-                func(request=request, **request_data.kwargs()), response_class
-            )
-
-    elif issubclass(request_model, BaseModel):
-
-        def _endpoint(
-            request: Request,
-            request_data: request_model,  # type:ignore
-        ):
-            """Endpoint."""
-            return _wrap_response(func(request_data, request=request), response_class)
-
-    else:
-
-        def _endpoint(
-            request: Request,
-            request_data: Dict[str, Any],  # type:ignore
-        ):
-            """Endpoint."""
-            return _wrap_response(func(request_data, request=request), response_class)
 
     return _endpoint
 
