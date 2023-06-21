c0183cffef06db94bcdc3c2b01b79364172ea731
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2023-06-21 23:52:07 +0200

    Allowing to fix the request base URL via an env var

---
+++ stac_fastapi/types/tests/test_requests.py
@@ -0,0 +1,13 @@
+import os
+from unittest import mock
+
+from starlette.requests import Request
+
+from stac_fastapi.types.requests import get_base_url
+
+
+def test_get_base_url_with_env():
+    env = {'REQUEST_BASE_URL': 'http://example.com/path/'}
+    with mock.patch.dict(os.environ, env):
+        print(os.environ)
+        assert get_base_url(Request({"type": "http", "app": None})) == "http://example.com/path/"
