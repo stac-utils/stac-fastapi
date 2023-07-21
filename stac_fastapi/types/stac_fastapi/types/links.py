d8cbd628de9e1789cb27943fbfda75240cd4a577
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2023-07-21 15:17:34 +0200

    chore: change base_url til custome hrefbuilder to add token on URL's

--- stac_fastapi/types/stac_fastapi/types/links.py
+++ stac_fastapi/types/stac_fastapi/types/links.py
@@ -1,7 +1,6 @@
 """Link helpers."""
-
 from typing import Any, Dict, List
-from urllib.parse import urljoin
+from urllib.parse import urlencode, urljoin
 
 import attr
 from stac_pydantic.links import Relations
@@ -18,6 +17,7 @@ def filter_links(links: List[Dict]) -> List[Dict]:
     return [link for link in links if link["rel"] not in INFERRED_LINK_RELS]
 
 
+# Har vi behov for denne?
 def resolve_links(links: list, base_url: str) -> List[Dict]:
     """Convert relative links to absolute links."""
     filtered_links = filter_links(links)
@@ -27,15 +27,35 @@ def resolve_links(links: list, base_url: str) -> List[Dict]:
 
 
 @attr.s
+class BaseHrefBuilder:
+    """Builds hrefs for service links by extending service base url.
+
+    Adds `token` param to all hrefs.
+
+    Remember that token must be in a Dict as in `q["token"]`.
+    """
+
+    base_url: str = attr.ib()
+    token: str = attr.ib()
+
+    def build(self, path: str = None, query: Dict[str, str] = None):
+        # with_path = urljoin(self.base_url, path)
+        return urljoin(path, "?" + urlencode(query)) if query else path
+
+
+@attr.s
 class BaseLinks:
     """Create inferred links common to collections and items."""
 
     collection_id: str = attr.ib()
-    base_url: str = attr.ib()
+    # base_url: str = attr.ib()
+    href_builder: BaseHrefBuilder = attr.ib()
 
     def root(self) -> Dict[str, Any]:
         """Return the catalog root."""
-        return dict(rel=Relations.root, type=MimeTypes.json, href=self.base_url)
+        return dict(
+            rel=Relations.root, type=MimeTypes.json, href=self.href_builder.base_url
+        )
 
 
 @attr.s
@@ -47,19 +67,25 @@ class CollectionLinks(BaseLinks):
         return dict(
             rel=Relations.self,
             type=MimeTypes.json,
-            href=urljoin(self.base_url, f"collections/{self.collection_id}"),
+            # href=urljoin(self.base_url, f"collections/{self.collection_id}"),
+            href=urljoin(
+                self.href_builder.base_url, f"collections/{self.collection_id}"
+            ),
         )
 
     def parent(self) -> Dict[str, Any]:
         """Create the `parent` link."""
-        return dict(rel=Relations.parent, type=MimeTypes.json, href=self.base_url)
+        return dict(rel=Relations.parent, type=MimeTypes.json, href=self.href_builder)
 
     def items(self) -> Dict[str, Any]:
         """Create the `items` link."""
         return dict(
             rel="items",
             type=MimeTypes.geojson,
-            href=urljoin(self.base_url, f"collections/{self.collection_id}/items"),
+            # href=urljoin(self.base_url, f"collections/{self.collection_id}/items"),
+            href=urljoin(
+                self.href_builder.base_url, f"collections/{self.collection_id}/items"
+            ),
         )
 
     def create_links(self) -> List[Dict[str, Any]]:
@@ -78,26 +104,44 @@ class ItemLinks(BaseLinks):
         return dict(
             rel=Relations.self,
             type=MimeTypes.geojson,
+            # href=urljoin(
+            #    self.base_url,
+            #    f"collections/{self.collection_id}/items/{self.item_id}",
+            # ),
             href=urljoin(
-                self.base_url,
+                self.href_builder.base_url,
                 f"collections/{self.collection_id}/items/{self.item_id}",
             ),
         )
 
     def parent(self) -> Dict[str, Any]:
         """Create the `parent` link."""
+        # return dict(
+        #    rel=Relations.parent,
+        #    type=MimeTypes.json,
+        #    href=urljoin(self.base_url, f"collections/{self.collection_id}"),
+        # )
         return dict(
             rel=Relations.parent,
             type=MimeTypes.json,
-            href=urljoin(self.base_url, f"collections/{self.collection_id}"),
+            href=urljoin(
+                self.href_builder.base_url, f"collections/{self.collection_id}"
+            ),
         )
 
     def collection(self) -> Dict[str, Any]:
         """Create the `collection` link."""
+        # return dict(
+        #    rel=Relations.collection,
+        #    type=MimeTypes.json,
+        #    href=urljoin(self.base_url, f"collections/{self.collection_id}"),
+        # )
         return dict(
             rel=Relations.collection,
             type=MimeTypes.json,
-            href=urljoin(self.base_url, f"collections/{self.collection_id}"),
+            href=urljoin(
+                self.href_builder.base_url, f"collections/{self.collection_id}"
+            ),
         )
 
     def create_links(self) -> List[Dict[str, Any]]:
