f625b24b3c8d448d608d3d07eb63c71e8079524b
Vincent Sarago <vincent.sarago@gmail.com>
2024-04-09 20:29:40 +0200

    Update stac_fastapi/types/tests/test_requests.py
    Co-authored-by: Pete Gadomski <pete.gadomski@gmail.com>

--- stac_fastapi/types/tests/test_requests.py
+++ stac_fastapi/types/tests/test_requests.py
@@ -9,5 +9,4 @@ from stac_fastapi.types.requests import get_base_url
 def test_get_base_url_with_env():
     env = {'REQUEST_BASE_URL': 'http://example.com/path/'}
     with mock.patch.dict(os.environ, env):
-        print(os.environ)
         assert get_base_url(Request({"type": "http", "app": None})) == "http://example.com/path/"
