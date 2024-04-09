739bab93144e4eed1b8ea6f75c1fccebb06ef1b9
vincentsarago <vincent.sarago@gmail.com>
2024-04-09 20:34:10 +0200

    lint

--- stac_fastapi/types/tests/test_requests.py
+++ stac_fastapi/types/tests/test_requests.py
@@ -7,6 +7,9 @@ from stac_fastapi.types.requests import get_base_url
 
 
 def test_get_base_url_with_env():
-    env = {'REQUEST_BASE_URL': 'http://example.com/path/'}
+    env = {"REQUEST_BASE_URL": "http://example.com/path/"}
     with mock.patch.dict(os.environ, env):
-        assert get_base_url(Request({"type": "http", "app": None})) == "http://example.com/path/"
+        assert (
+            get_base_url(Request({"type": "http", "app": None}))
+            == "http://example.com/path/"
+        )
