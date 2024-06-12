9a8ab844d9d3ceb4a258e112a4328a8ddabe27c1
rhysrevans3 <34507919+rhysrevans3@users.noreply.github.com>
2024-06-12 08:43:31 +0100

    Allow default route dependencies (#705)
    * Allowing for default route dependencies.
    
    * Running precommit hooks.
    
    * Adding pull request to CHANGELOG.
    
    * Update stac_fastapi/api/stac_fastapi/api/routes.py
    
    Co-authored-by: Anthony Lukach <anthonylukach@gmail.com>
    
    * Fixing indenting.
    
    ---------
    
    Co-authored-by: Jonathan Healy <jonathan.d.healy@gmail.com>
    Co-authored-by: Anthony Lukach <anthonylukach@gmail.com>

--- stac_fastapi/api/stac_fastapi/api/routes.py
+++ stac_fastapi/api/stac_fastapi/api/routes.py
@@ -1,5 +1,6 @@
 """Route factories."""
 
+import copy
 import functools
 import inspect
 import warnings
@@ -100,13 +101,26 @@ def add_route_dependencies(
     Allows a developer to add dependencies to a route after the route has been
     defined.
 
+    "*" can be used for path or method to match all allowed routes.
+
     Returns:
         None
     """
     for scope in scopes:
+        _scope = copy.deepcopy(scope)
         for route in routes:
-            match, _ = route.matches({"type": "http", **scope})
+            if scope["path"] == "*":
+                _scope["path"] = route.path
+
+            if scope["method"] == "*":
+                _scope["method"] = list(route.methods)[0]
+
+            match, _ = route.matches({"type": "http", **_scope})
             if match != Match.FULL:
+                continue
+
+            # Ignore paths without dependants, e.g. /api, /api.html, /docs/oauth2-redirect
+            if not hasattr(route, "dependant"):
                 continue
 
             # Mimicking how APIRoute handles dependencies:
