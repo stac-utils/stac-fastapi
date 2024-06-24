e14a1081a3f5a9abc20765d034ac80efb4e23bd4
James <jafisher32@gmail.com>
2024-06-24 17:14:26 -0400

    remove filter extension dependency

--- stac_fastapi/extensions/stac_fastapi/extensions/core/aggregation/request.py
+++ stac_fastapi/extensions/stac_fastapi/extensions/core/aggregation/request.py
@@ -4,21 +4,17 @@ from typing import List, Optional, Union
 
 import attr
 
-from stac_fastapi.extensions.core.filter.request import (
-    FilterExtensionGetRequest,
-    FilterExtensionPostRequest,
-)
 from stac_fastapi.types.search import BaseSearchGetRequest, BaseSearchPostRequest, str2list
 
 
 @attr.s
-class AggregationExtensionGetRequest(BaseSearchGetRequest, FilterExtensionGetRequest):
+class AggregationExtensionGetRequest(BaseSearchGetRequest):
     """Aggregation Extension GET request model."""
 
     aggregations: Optional[str] = attr.ib(default=None, converter=str2list)
 
 
-class AggregationExtensionPostRequest(BaseSearchPostRequest, FilterExtensionPostRequest):
+class AggregationExtensionPostRequest(BaseSearchPostRequest):
     """Aggregation Extension POST request model."""
 
     aggregations: Optional[List[str]] = attr.ib(default=None)
