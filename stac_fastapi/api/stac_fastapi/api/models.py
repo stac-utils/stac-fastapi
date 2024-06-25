3003eed294d42953619d851a6eb46e50766eff87
vincentsarago <vincent.sarago@gmail.com>
2024-06-25 19:44:37 +0200

    remove models from api

--- stac_fastapi/api/stac_fastapi/api/models.py
+++ stac_fastapi/api/stac_fastapi/api/models.py
@@ -110,32 +110,6 @@ class ItemCollectionUri(CollectionUri):
     datetime: Optional[DateTimeType] = attr.ib(default=None, converter=str_to_interval)
 
 
-class POSTTokenPagination(BaseModel):
-    """Token pagination model for POST requests."""
-
-    token: Optional[str] = None
-
-
-@attr.s
-class GETTokenPagination(APIRequest):
-    """Token pagination for GET requests."""
-
-    token: Optional[str] = attr.ib(default=None)
-
-
-class POSTPagination(BaseModel):
-    """Page based pagination for POST requests."""
-
-    page: Optional[str] = None
-
-
-@attr.s
-class GETPagination(APIRequest):
-    """Page based pagination for GET requests."""
-
-    page: Optional[str] = attr.ib(default=None)
-
-
 # Test for ORJSON and use it rather than stdlib JSON where supported
 if importlib.util.find_spec("orjson") is not None:
     from fastapi.responses import ORJSONResponse
