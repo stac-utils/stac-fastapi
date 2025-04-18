cb0013dd5372a87ef502c7ce70f3aff15d84780f
vincentsarago <vincent.sarago@gmail.com>
2025-04-18 10:25:54 +0200

    update sub-module requirements

--- stac_fastapi/extensions/setup.py
+++ stac_fastapi/extensions/setup.py
@@ -7,8 +7,8 @@ with open("README.md") as f:
     desc = f.read()
 
 install_requires = [
-    "stac-fastapi.types~=5.0",
-    "stac-fastapi.api~=5.0",
+    "stac-fastapi.types~=5.2",
+    "stac-fastapi.api~=5.2",
 ]
 
 extra_reqs = {
