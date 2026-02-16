1746daabf5a6662ee94d9f8047c31a3d40ac537f
hrodmn <henry.rodman@gmail.com>
2026-02-16 14:57:17 -0600

    chore: fix formatting again

--- stac_fastapi/api/tests/test_api.py
+++ stac_fastapi/api/tests/test_api.py
@@ -39,9 +39,9 @@ class TestRouteDependencies:
         with TestClient(api.app) as client:
             for route in routes:
                 response = getattr(client, route["method"].lower())(route["path"])
-                assert response.status_code == 401, (
-                    "Unauthenticated requests should be rejected"
-                )
+                assert (
+                    response.status_code == 401
+                ), "Unauthenticated requests should be rejected"
                 assert response.json() == {"detail": "Not authenticated"}
 
                 path = route["path"].format(
@@ -54,9 +54,9 @@ class TestRouteDependencies:
                     content=route["payload"],
                     headers={"content-type": "application/json"},
                 )
-                assert 200 <= response.status_code < 300, (
-                    "Authenticated requests should be accepted"
-                )
+                assert (
+                    200 <= response.status_code < 300
+                ), "Authenticated requests should be accepted"
                 assert response.json() == "dummy response"
 
     @staticmethod
@@ -73,9 +73,9 @@ class TestRouteDependencies:
                     headers={"content-type": "application/json"},
                 )
 
-                assert 200 <= response.status_code < 300, (
-                    "Unauthenticated requests should be accepted"
-                )
+                assert (
+                    200 <= response.status_code < 300
+                ), "Unauthenticated requests should be accepted"
                 assert response.json() == "dummy response"
 
     def test_openapi_content_type(self):
